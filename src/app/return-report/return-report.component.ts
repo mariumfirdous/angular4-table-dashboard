c5e2c9f9c192cf6d3ebfd9b9b6aa8fccae6b514a
Marium Firdous Kazmi <MF00561052@techmahindra.com>
2018-08-13 17:11:44 +0530

    returnData updated

--- src/app/return-report/return-report.component.ts
+++ src/app/return-report/return-report.component.ts
@@ -15,6 +15,7 @@ export class ReturnReportComponent implements OnInit {
   defaultTable : boolean = true;
   sortedData :  any=[];
   random : number;
+  monthTable : boolean = false;
   constructor(private myService: DataPassService) { 
 
   }
@@ -24,7 +25,7 @@ export class ReturnReportComponent implements OnInit {
     this.return=this.myService.return;
   }
   sortedTable(value: string){
-    this.yearTable = true;
+    
     console.log("sorted");
     this.defaultTable = false;
     console.log(this.yearTable);
@@ -34,7 +35,7 @@ export class ReturnReportComponent implements OnInit {
     console.log("sort");
     if( value === 'yearTab'){
       console.log("first if");
-      
+      this.yearTable = true;
       console.log(JSON.stringify(this.sortedData));
       
       for (var  i=0; i<this.returnData.length; i++){
@@ -44,12 +45,35 @@ export class ReturnReportComponent implements OnInit {
         console.log(this.returnData[i].year);
         if(this.random == -1){
           this.sortedData.push(this.returnData[i]);
-          console.log(this.sortedData[i].total);
+         
         }
         else {         
           this.sortedData[this.random].total= this.sortedData[this.random].total + this.returnData[i].total; 
+          
         }
       }
+      return this.sortedData;
+    }
+    if( value === 'monthTab'){
+      console.log("first if");
+      this.monthTable = true;
+      console.log(JSON.stringify(this.sortedData));
+      
+      for (var  i=0; i<this.returnData.length; i++){
+        this.random=0;
+        this.checkSortedDataMonth(this.returnData[i].month);
+        console.log(this.random);
+        console.log(this.returnData[i].month);
+        if(this.random == -1){
+          this.sortedData.push(this.returnData[i]);
+         
+        }
+        else {         
+          this.sortedData[this.random].total= this.sortedData[this.random].total + this.returnData[i].total; 
+          
+        }
+      }
+      return this.sortedData;
     }
   }
 checkSortedData(num: number){
@@ -62,52 +86,76 @@ checkSortedData(num: number){
       return this.random;
     }  
   }
+ 
   
     this.random = -1;
     return this.random;
   
 }
 
+checkSortedDataMonth(num: number){
+  for( var i=0; i<this.sortedData.length ; i++){
+    console.log(num);
+     if( this.sortedData[i].month == num){
+       this.random = i;
+       console.log("number");
+       console.log(num );
+       return this.random;
+     }  
+  }
+  this.random = -1;
+    return this.random;
+  
+}
+
+
 
 returnData = [
   { "date":25,
   "month": 4,
+  "Month": "April",
   "year": 2018,
     "total": 2
   },
   {
     "date":18,
     "month": 10,
+    "Month": "October",
     "year": 2017,
     "total": 3
   },
   {
     "date":10,
     "month": 6,
+    "Month": "July",
     "year": 2018,
     "total": 1
   },
   {
     "date":4,
     "month": 1,
+    "Month": "January",
     "year": 2017,
     "total":4
   },
   {
     "date":30,
     "month": 8,
+    "Month": "August",
     "year": 2016,
     "total":6
   },
   {
     "date":30,
     "month": 8,
+    "Month": "August",
     "year": 2017,
     "total":4
   },
   {
     "date":30,
     "month": 8,
+    "Month": "August",
     "year": 2018,
     "total":2
   }
